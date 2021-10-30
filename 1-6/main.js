4d85ff8818e0e31dbe3933e24b1b890cefea3d80
yuka tsubaki <icdeuty4jb@gmail.com>
2021-10-30 21:44:49 +0900

    課題提出js1-6
    追加（問2）

--- 1-6/main.js
+++ 1-6/main.js
@@ -1,3 +1,5 @@
+//問1
+
 let scores = [10, 15, 20, 25];
 console.log(scores);
 for (let i = 0; i < scores.length ; i++){
@@ -5,3 +7,13 @@ if (scores[i] % 2 === 0) {
     console.log(scores[i] + "は偶数です");
     }  
 }
+
+
+//問2
+
+let car = {
+  gass: 20.5,
+  num:  1234,
+};
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
