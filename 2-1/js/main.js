36a2258d30c19362928c03a2b0f244df537f1274
yuka tsubaki <icdeuty4jb@gmail.com>
2021-11-06 11:05:39 +0900

    課題提出js2-1
    getElementByIdの使用

---
+++ 2-1/js/main.js
@@ -0,0 +1,23 @@
+// let elems = document.getElementsByTagName('h1');
+
+// console.log(elems[0]);
+// console.log(elems[1]);
+
+// let boxes = document.getElementsByName('box');
+
+// for (let i = 0; i < boxes.length; i++) {
+//     console.log(boxes[i]);
+// }
+
+// let boxes2 = document.getElementsByClassName('box');
+
+// console.log(boxes2[0]);
+// console.log(boxes2[2]);
+// console.log(boxes2[3]);
+
+
+
+function changeColor() {
+    let box = document.getElementById('box');
+    box.style.backgroundColor = "red";
+}
